4471bd67bd1dc65b661e4aacabed8027b066f1db
abiyevseymur <abiyevseymur92@gmail.com>
2019-09-16 20:45:25 +0400

    Updates

---
+++ precache-manifest.d43e68e6971263a05963fb726045c6f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c80b9b28752ce3f606bb348717e264b",
+    "url": "/animated-cards/index.html"
+  },
+  {
+    "revision": "e8ae0a227a6c074f4078",
+    "url": "/animated-cards/static/css/main.fa59d73b.chunk.css"
+  },
+  {
+    "revision": "30558e99711ae1df7052",
+    "url": "/animated-cards/static/js/2.ec39e42d.chunk.js"
+  },
+  {
+    "revision": "e8ae0a227a6c074f4078",
+    "url": "/animated-cards/static/js/main.e9621a5f.chunk.js"
+  },
+  {
+    "revision": "c2798d2a3a4c4c1378d9",
+    "url": "/animated-cards/static/js/runtime~main.0e5133d1.js"
+  }
+]);
